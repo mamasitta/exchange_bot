0507a6b24eaeaec6c55888645ec315527a347899
Victoria <vikiber2013@gmail.com>
2021-02-17 22:05:20 +0200

    add '/lst' command as optional to '/list'

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import psycopg2
 
 TOKEN = '1533284782:AAESnjHJ7l6pJ-fz_i7oqJoS8ttug9N5A3Q'
 
-db = psycopg2.connect('postgres://pfcqrhowehtmbz:fbdaee48925562e654603f92e3f867e09ab38e12370bafb845c764cde591b797@ec2-52-70-135-246.compute-1.amazonaws.com:5432/d61ji76alun00r')
+db = psycopg2.connect()
